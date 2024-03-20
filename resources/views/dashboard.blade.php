4dbe3fb5ff61e43b20f215e8c1b5fbf2a395bbf1
Mehna <mehnafathima777@gmail.com>
2024-03-20 23:29:04 +0530

    20324commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,7 +14,7 @@
                     <p class="pb-0">Best seller of the month</p>
                     <h4 class="text-primary mb-1">$42.8k</h4>
                     <p class="mb-2 pb-1">78% of target 🚀</p>
-                    <a href="javascript:;" class="btn btn-sm btn-primary">View Data</a>
+                    <a href="javascript:;" class="btn btn-sm btn-primary">View Details</a>
                   </div>
                   <img src="{{ asset('assets/img/icons/misc/triangle-light.png') }}" class="scaleX-n1-rtl position-absolute bottom-0 end-0" width="166" alt="triangle background" data-app-light-img="icons/misc/triangle-light.png') }}" data-app-dark-img="icons/misc/triangle-dark.png') }}" />
                   <img src="{{ asset('assets/img/illustrations/trophy.png') }}" class="scaleX-n1-rtl position-absolute bottom-0 end-0 me-4 mb-4 pb-2" width="83" alt="view sales" />
